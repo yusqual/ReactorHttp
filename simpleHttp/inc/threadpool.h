e4811ab68713c3433cfc05f98dde4696077c5a1f
Yusqual <yusqual@gmail.com>
2024-08-19 01:03:11 +0800

    简化头文件

--- simpleHttp/inc/threadpool.h
+++ simpleHttp/inc/threadpool.h
@@ -4,12 +4,7 @@
 #ifndef _THREADPOOL_H_
 #define _THREADPOOL_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <errno.h>
-#include <unistd.h>
+#include <myheads/base.h>
 #include <pthread.h>
 #include <semaphore.h>
 
